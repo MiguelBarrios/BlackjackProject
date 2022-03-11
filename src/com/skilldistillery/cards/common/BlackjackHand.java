78a39ba7f01887873e7b3984e8180b51a135969b
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-03-11 16:05:24 -0700

    hand and blackjack hand complete

---
+++ src/com/skilldistillery/cards/common/BlackjackHand.java
@@ -0,0 +1,41 @@
+package com.skilldistillery.cards.common;
+
+public class BlackjackHand extends Hand {
+	
+	public BlackjackHand() {
+		super();
+	}
+
+	@Override
+	public int getHandValue() {
+		int value = 0;
+		for(Card card : cards) {
+			value += card.getValue();
+		}
+		
+		return value;
+	}
+	
+	public boolean isBlackjack() {
+		
+		int value = getHandValue();
+		return (value == 21 && cards.size() == 2);
+	}
+	
+	public boolean isBust() {
+		return (getHandValue() > 21);
+	}
+	
+	public boolean isSoft() {
+		for(Card card : cards) {
+			if(card.getValue() == 11);
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean isHard() {
+		return !isSoft();
+	}
+
+}
---
+++ src/com/skilldistillery/cards/common/Hand.java
@@ -0,0 +1,31 @@
+package com.skilldistillery.cards.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Hand {
+	
+	protected List<Card> cards;
+	
+	public abstract int getHandValue();
+	
+	public Hand() {
+		cards = new ArrayList<>();
+	}
+	
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	public void clear() {
+		cards.clear();
+	}
+
+	@Override
+	public String toString() {
+		return "Hand [cards=" + cards + "]";
+	}
+	
+	
+	
+}
